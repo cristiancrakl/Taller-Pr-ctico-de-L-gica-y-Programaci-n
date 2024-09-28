bbd64202a1739229f5890e94bda72bd68ca37497
cristiancrakl <moralescadenac@gmail.com>
2024-09-28 00:26:58 -0500

    acabe de termiar lo de fizz buzz falta optimizarlo mas

--- Fizz_Buzz.java
+++ Fizz_Buzz.java
@@ -1,27 +1,27 @@
 public class Fizz_Buzz {
     public static void main(String[] args) {
-        
 
+        for (int i = 1; i <= 100; i++) {
 
+            if (i % 3 == 0) {
 
+                System.out.println("Fizz");
+                continue;
+            } else {
+                if (i % 5 == 0) {
+                    System.out.println("Buzz");
+                    continue;
+                } else {
+                    if (i % 5 == 0 && i % 3 == 0) {
+                        System.out.println("FizzBuzz");
+                        continue;
+                    } else {
+                        System.out.println(i);
 
-        for (int i = 0; i == 100; i++) {
-            System.out.println(i);
+                    }
+                }
+            }
         }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
     }
 
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 taller practico
+
+
